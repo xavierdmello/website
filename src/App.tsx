95518415ae3cc04417948e608fcb0b9ab19bc50b
Xavier D'Mello <xaviermdmello@gmail.com>
2023-07-04 18:58:53 -0400

    flex padding

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,17 @@ function App() {
     <div>
       {/* Background */}
       <div className="fixed right-0 -z-1 float-right w-3/4 h-full bg-background"></div>
+      <Header />
 
-      <div className="fixed m-4">
-        <Header />
-        <h4>INTRO</h4>
-        <h1 className="text-3xl max-w-2xl 2xl:text-6xl 2xl:max-w-5xl">Hi, I'm Xavier, a Full-Stack Developer based out of Waterloo, Canada.</h1>
-        <Link text="About me" href="https://www.xavierdmello.com/about" />
+      <div className="fixed flex flex-row w-full">
+        {/* Left Side */}
+        <div className="leftSide">
+          <h4 className="mb-6">INTRO</h4>
+          <h1 className="2xl:mb-12 mb-6 text-3xl max-w-2xl 2xl:text-6xl 2xl:max-w-5xl">
+            Hi, I'm Xavier, a Full-Stack Developer based out of Waterloo, Canada.
+          </h1>
+          <Link text="About me" href="https://www.xavierdmello.com/about" />
+        </div>
       </div>
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,18 @@ import Menu from "./Menu";
 export default function Header() {
   return (
     <div>
-      <div className="fixed pr-8 w-full flex flex-row justify-between">
+      <div className="fixed p-4 w-full flex flex-row justify-between">
         {/* Logo */}
-        <a href="https://www.xavierdmello.com" className="w-12 h-12 sm:w-24 sm:h-24  bg-primary grid place-content-center hover:bg-black hover:text-white duration-500 transition-all">
+        <a
+          href="https://www.xavierdmello.com"
+          className="w-12 h-12 sm:w-24 sm:h-24 bg-primary grid place-content-center hover:bg-black hover:text-white duration-500 transition-all"
+        >
           <h1 className="text-2xl font-bold">xd.</h1>
         </a>
 
         <Menu />
       </div>
-      <div className="h-24 sm:h-48"></div>
+      <div className="h-24 sm:h-44"></div>
     </div>
   );
 }
