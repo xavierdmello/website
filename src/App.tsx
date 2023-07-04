53f1dddbcc8b9e89226127d14f2596e6419788fe
Xavier D'Mello <xaviermdmello@gmail.com>
2023-07-04 16:46:57 -0400

    update logo

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ function App() {
   return (
     <div className="m-4">
       <Header />
-      <h1 className="text-3xl max-w-2xl 2xl:text-6xl 2xl:max-w-5xl">Hi, I'm Xavier, a Full-Stack Developer based out of Waterloo, Canada.</h1>
-      <b>Check out the app</b>
       <h4>INTRO</h4>
+      <h1 className="text-3xl max-w-2xl 2xl:text-6xl 2xl:max-w-5xl">Hi, I'm Xavier, a Full-Stack Developer based out of Waterloo, Canada.</h1>
+      <b>About me</b>
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,11 @@ import Menu from "./Menu"
 export default function Header() {
     return (
       <div className="box-border">
-        <div className="fixed pr-8 box-border w-full flex flex-row justify-between">
-          <div className="w-24 h-24 bg-primary grid place-content-center">
+        <div className="fixed pr-8 w-full flex flex-row justify-between">
+          <div className="w-12 h-12 sm:w-24 sm:h-24  bg-primary grid place-content-center">
             <h1 className="text-2xl font-bold">xd.</h1>
           </div>
-          <Menu/>
+          <Menu />
         </div>
         <div className="h-48"></div>
       </div>
