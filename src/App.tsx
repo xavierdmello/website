14d57841953875cea04fe4490505c4a54aac99ab
Xavier D'Mello <xaviermdmello@gmail.com>
2023-07-04 19:05:06 -0400

    dynamic padding

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ function App() {
       <div className="fixed right-0 -z-1 float-right w-3/4 h-full bg-background"></div>
       <Header />
 
-      <div className="fixed flex flex-row w-full">
+      <div className="fixed flex flex-row w-full dynamicPadding">
         {/* Left Side */}
-        <div className="leftSide">
+        <div>
           <h4 className="mb-6">INTRO</h4>
           <h1 className="2xl:mb-12 mb-6 text-2xl sm:text-3xl max-w-2xl 2xl:text-6xl 2xl:max-w-5xl">
             Hi, I'm Xavier, a Full-Stack Developer based out of Waterloo, Canada.
