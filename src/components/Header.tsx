32affd316fe5ba87fd2717c860ec72bbf66fc9fc
Xavier D'Mello <xaviermdmello@gmail.com>
2023-07-04 13:56:16 -0400

    fix colour

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
     return (
       <>
         <div className="fixed w-full flex flex-row justify-between">
-          <div className="m-4 w-28 h-28 bg-orange grid place-content-center">
+          <div className="m-4 w-28 h-28 bg-tan grid place-content-center">
             <h1 className="text-2xl font-bold">xd.</h1>
           </div>
           <h1 className="">bye</h1>
