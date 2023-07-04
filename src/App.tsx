9ea28bc32f6408c075255346c16d33a5c0db6643
Xavier D'Mello <xaviermdmello@gmail.com>
2023-07-04 19:02:21 -0400

    mobile friendly text

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         {/* Left Side */}
         <div className="leftSide">
           <h4 className="mb-6">INTRO</h4>
-          <h1 className="2xl:mb-12 mb-6 text-3xl max-w-2xl 2xl:text-6xl 2xl:max-w-5xl">
+          <h1 className="2xl:mb-12 mb-6 text-2xl sm:text-3xl max-w-2xl 2xl:text-6xl 2xl:max-w-5xl">
             Hi, I'm Xavier, a Full-Stack Developer based out of Waterloo, Canada.
           </h1>
           <Link text="About me" href="https://www.xavierdmello.com/about" />
