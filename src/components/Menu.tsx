459ae3b93e390333509870667e88653d54e6184f
Xavier D'Mello <xaviermdmello@gmail.com>
2023-07-04 16:39:16 -0400

    animated menu button

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,22 +1,14 @@
 import { useState } from "react";
 export default function Menu() {
-  const [menu, setMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
   return (
-    <div className={`w-12 h-12 justify-center items-center flex flex-col bg-primary`} onClick={() => setMenu(!menu)}>
+    <div className={`w-12 h-12 justify-center items-center flex flex-col bg-primary`} onClick={() => setShowMenu(!showMenu)}>
       <div className="flex flex-col gap-2">
-        <Bar menu={menu} />
-        <SmallBar menu={menu} />
-        <Bar menu={menu} />
+        <div className={`w-9 h-[3px] transition-all  bg-black ${showMenu && "rotate-45 translate-y-[10.5px]"}`}></div>
+        <div className={`w-7 h-[2px] transition-all bg-black ${showMenu && "opacity-0"}`}></div>
+        <div className={`w-9 h-[3px] transition-all bg-black ${showMenu && "-rotate-45 -translate-y-[10.5px]"}`}></div>
       </div>
     </div>
   );
-}
-
-function Bar(props: {menu: boolean}) {
-    return <div className={`h-[3px] transition-all duration-300 bg-black box-border ${props.menu && "scale-150"}`}></div>;
-}
-
-function SmallBar(props: {menu: boolean}) {
-    return <div className="w-7 h-[2px] bg-black box-border transition-all"></div>;
 }
