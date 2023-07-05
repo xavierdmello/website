19d51b749524a119bac4f81ecceea1bcf753527a
Xavier D'Mello <xaviermdmello@gmail.com>
2023-07-05 02:13:52 -0400

    links open in new tab

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -5,22 +5,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Socials() {
   return (
     <div>
-      <Codec text="ELSEWHERE" className="mb-6"/>
+      <Codec text="ELSEWHERE" className="mb-6" />
       <div className="w-fit fade2 grid grid-cols-3 gap-4">
-        <a href="https://www.linkedin.com/in/xavier-d-mello-552276240/">
+        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/xavier-d-mello-552276240/ ">
           <FontAwesomeIcon className="w-10 h-10 transition-all duration-300 hover:text-linkedin" icon={faLinkedin} />
         </a>
-        <a href="https://github.com/xavierdmello">
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/xavierdmello">
           <FontAwesomeIcon className="w-10 h-10 transition-all duration-300 hover:text-github" icon={faGithubSquare} />
         </a>
 
-        <a href="https://t.me/novaholonova">
+        <a target="_blank" rel="noopener noreferrer" href="https://t.me/novaholonova">
           <FontAwesomeIcon className="w-10 h-10 transition-all duration-300 hover:text-telegram" icon={faTelegram} />
         </a>
-        <a href="https://www.instagram.com/xavier.810/">
+        <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/xavier.810/">
           <FontAwesomeIcon className="w-10 h-10 transition-all duration-300 hover:text-instagram" icon={faInstagramSquare} />
         </a>
-        <a href="https://twitter.com/nova_holo">
+        <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/nova_holo">
           <FontAwesomeIcon className="w-10 h-10 transition-all duration-300 hover:text-twitter" icon={faTwitterSquare} />
         </a>
       </div>
