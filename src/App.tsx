aa936a84fa965b6a0103927163fe3057e4f98c5f
Xavier D'Mello <xaviermdmello@gmail.com>
2023-07-12 01:32:07 -0400

    bg update

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ import Codec from "./components/Codec";
 import Giant from "./components/Giant";
 
 // image imports
-import experience from "./assets/experience.png";
+import experience from "./assets/experience.jpg";
 import hackathons from "./assets/hackathons.png";
 import projects from "./assets/projects.png";
-import contacts from "./assets/contacts.png";
+import contacts from "./assets/contacts.jpg";
 
 function App() {
   const [bg, setBg] = useState("bg-background");
@@ -24,22 +24,6 @@ function App() {
       <BgImage image={projects} name="projects" active={bg === "bg-projects"} />
       <BgImage image={contacts} name="contacts" active={bg === "bg-contacts"} />
 
-      <img
-        src={hackathons}
-        alt="hackathons"
-        className={`fixed right-0 -z-10 float-right transition-all duration-1000 w-3/4 h-full opacity-0 ${bg === "bg-hackathons" && "opacity-100"}`}
-      />
-      <img
-        src={projects}
-        alt="projects"
-        className={`fixed right-0 -z-10 float-right transition-all duration-1000 w-3/4 h-full opacity-0 ${bg === "bg-projects" && "opacity-100"}`}
-      />
-      <img
-        src={contacts}
-        alt="contacts"
-        className={`fixed right-0 -z-10 float-right transition-all duration-1000 w-3/4 h-full opacity-0 ${bg === "bg-contacts" && "opacity-100"}`}
-      />
-
       <Header />
 
       <div className="flex flex-row w-full p-4 justify-between sm:dynamicPadding">
@@ -47,7 +31,7 @@ function App() {
           {/* Left Side */}
           <Codec text="INTRO" className="mb-6" />
           <div className="slide2">
-            <h1 className="3xl:mb-12  mb-6 text-2xl sm:text-3xl max-w-2xl 3xl:text-6xl 2xl:max-w-5xl">
+            <h1 className="3xl:mb-12 z-10 mb-6 relative text-2xl sm:text-3xl max-w-2xl 3xl:text-6xl 2xl:max-w-5xl">
               Hi, I'm Xavier, a Full-Stack Developer based out of Waterloo, Canada.
             </h1>
             <Link text="About me" href="https://www.xavierdmello.com/about" />
@@ -68,7 +52,7 @@ function App() {
           <p className="fade2">tacocat9293</p>
         </div>
 
-        <div className="hidden md:block z-10">
+        <div className="hidden md:block">
           {/* Right Side */}
           <Codec text="MY WORK" className="mb-6 text-right" />
           <ul>
@@ -96,7 +80,7 @@ function BgImage(props: {image: string, name: string, active: boolean}) {
     <img
       src={props.image}
       alt={props.name}
-      className={`fixed right-0 -z-10 float-right transition-all duration-[1200ms] w-3/4 h-full opacity-0 ${props.active && "opacity-100"}`}
+      className={`fixed object-cover right-0 -z-10 float-right transition-all duration-[1200ms] w-3/4 h-full opacity-0 ${props.active && "opacity-50"}`}
     />
   );
 }
--- src/components/Giant.tsx
+++ src/components/Giant.tsx
@@ -7,7 +7,7 @@ export default function Giant(props: { text: string; className?: string; onHover
       onMouseLeave={() => props.onHover("bg-background")}
       className={props.className + ` group flex flex-row justify-end relative 3xl:text-8xl 3xl:mb-11 mb-7 text-5xl font-bold text-right transition-all`}
     >
-      <div className="w-72 float-right h-7 top-7 flex- bg-primary opacity-0 text-right group-hover:opacity-100 absolute transition-all duration-300 -z-10"></div>
+      <div className="w-72 h-7 top-7 3xl:w-[600px] 3xl:h-10 3xl:top-14 float-right bg-primary opacity-0 text-right group-hover:opacity-100 absolute transition-all duration-300 -z-10"></div>
       {props.text}
     </h1>
   );
