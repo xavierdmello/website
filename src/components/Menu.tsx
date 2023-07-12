7a12e2329929f2a93f74910db04e4f2a84f1c340
Xavier DMello <xaviermdmello@gmail.com>
2023-07-12 14:46:23 -0400

    cursor no select

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,7 +3,7 @@ export default function Menu() {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
-    <div className={`w-12 h-12 justify-center items-center flex flex-col bg-primary hover:cursor-pointer`} onClick={() => setShowMenu(!showMenu)}>
+    <div className={`w-12 h-12 select-none justify-center items-center flex flex-col bg-primary hover:cursor-pointer`} onClick={() => setShowMenu(!showMenu)}>
       <div className="flex flex-col gap-2">
         <div className={`w-9 h-[3px] transition-all  bg-black ${showMenu && "rotate-45 translate-y-[10.5px]"}`}></div>
         <div className={`w-7 h-[2px] transition-all bg-black ${showMenu && "opacity-0"}`}></div>
