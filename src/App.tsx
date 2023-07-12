06a18a966b660d10822232c6395799f7ec51c3fa
Xavier D'Mello <xaviermdmello@gmail.com>
2023-07-11 23:58:39 -0400

    bg images

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,40 @@ import Socials from "./components/Socials";
 import Codec from "./components/Codec";
 import Giant from "./components/Giant";
 
+// image imports
+import experience from "./assets/experience.png";
+import hackathons from "./assets/hackathons.png";
+import projects from "./assets/projects.png";
+import contacts from "./assets/contacts.png";
+
 function App() {
   const [bg, setBg] = useState("bg-background");
 
   return (
     <div>
       {/* Background */}
-      <div className={`fixed right-0 fadeBg -z-10 float-right transition-all w-3/4 h-full ${bg}`}></div>
+      {/* Not using background-image because of bug with tailwind */}
+      <div className={`fixed right-0 fadeBg -z-10 float-right transition-all w-3/4 h-full bg-background`}></div>
+      <BgImage image={experience} name="experience" active={bg === "bg-experience"} />
+      <BgImage image={hackathons} name="hackathons" active={bg === "bg-hackathons"} />
+      <BgImage image={projects} name="projects" active={bg === "bg-projects"} />
+      <BgImage image={contacts} name="contacts" active={bg === "bg-contacts"} />
+
+      <img
+        src={hackathons}
+        alt="hackathons"
+        className={`fixed right-0 -z-10 float-right transition-all duration-1000 w-3/4 h-full opacity-0 ${bg === "bg-hackathons" && "opacity-100"}`}
+      />
+      <img
+        src={projects}
+        alt="projects"
+        className={`fixed right-0 -z-10 float-right transition-all duration-1000 w-3/4 h-full opacity-0 ${bg === "bg-projects" && "opacity-100"}`}
+      />
+      <img
+        src={contacts}
+        alt="contacts"
+        className={`fixed right-0 -z-10 float-right transition-all duration-1000 w-3/4 h-full opacity-0 ${bg === "bg-contacts" && "opacity-100"}`}
+      />
 
       <Header />
 
@@ -41,7 +68,7 @@ function App() {
           <p className="fade2">tacocat9293</p>
         </div>
 
-        <div>
+        <div className="z-10">
           {/* Right Side */}
           <Codec text="MY WORK" className="mb-6 text-right" />
           <ul>
@@ -64,4 +91,13 @@ function App() {
   );
 }
 
+function BgImage(props: {image: string, name: string, active: boolean}) {
+  return (
+    <img
+      src={props.image}
+      alt={props.name}
+      className={`fixed right-0 -z-10 float-right transition-all duration-[1200ms] w-3/4 h-full opacity-0 ${props.active && "opacity-100"}`}
+    />
+  );
+}
 export default App;
