04ad9f549dcc1bd196ba541f156c546df2519eb0
Xavier D'Mello <xaviermdmello@gmail.com>
2023-07-20 16:11:36 -0400

    finish sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,27 +1,29 @@
 import Giant from "./Giant";
-
 export default function Sidebar(props: {showSidebar: boolean}) {
-
     return (
-        <div
-          className={`fixed w-full sm:w-96 3xl:w-[800px] bg-white items-center ${
-            props.showSidebar ? "translate-x-0" : "translate-x-full"
-          } flex align-bottom justify-center h-full transition-all duration-[600ms] z-10`}
-        >
-          <ul>
-            <li>
-              <Giant text="Experience" />
-            </li>
-            <li>
-              <Giant text="Hackathons" />
-            </li>
-            <li>
-              <Giant text="Projects" />
-            </li>
-            <li>
-              <Giant text="Contact" />
-            </li>
-          </ul>
-        </div>
+      <div
+        className={`fixed w-full sm:w-96 3xl:w-[800px] bg-white  ${
+          props.showSidebar ? "translate-x-0" : "translate-x-full"
+        } flex flex-col justify-center items-center h-full transition-all duration-[600ms] z-10`}
+      >
+        <ul>
+
+          <li>
+            <Giant text="About" />
+          </li>
+          <li>
+            <Giant text="Experience" />
+          </li>
+          <li>
+            <Giant text="Hackathons" />
+          </li>
+          <li>
+            <Giant text="Projects" />
+          </li>
+          <li>
+            <Giant text="Contact" />
+          </li>
+        </ul>
+      </div>
     );
 }
