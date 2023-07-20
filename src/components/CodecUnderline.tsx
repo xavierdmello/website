4f80e0d527b9167d0e13743510e928836d3183a9
Xavier D'Mello <xaviermdmello@gmail.com>
2023-07-20 16:53:25 -0400

    underline

--- src/components/CodecUnderline.tsx
+++ src/components/CodecUnderline.tsx
@@ -1,9 +1,8 @@
 export default function CodecUnderline(props: { text: string; className?: string }) {
   return (
     <div className={props.className + " flex flex-row"}>
-      <h4 className={`relative left-3 bottom-2 fade1 transition-all duration-[2000ms]`}>{props.text}</h4>
-      <div className="relative bottom-[7px] left-[3px] rotate-45 h-[0.7px] w-1/12 bg-gray"></div>
-      <div className="h-[0.7px] w-11/12 bg-gray"></div>
+      <h4 className={`relative left-5 bottom-[27px]  fade1 transition-all duration-[2000ms]`}>{props.text}</h4>
+      <div className="h-[0.7px] w-[350px] bg-gray"></div>
     </div>
   );
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,29 +3,29 @@ import CodecUnderline from "./CodecUnderline";
 export default function Sidebar(props: {showSidebar: boolean}) {
     return (
       <div
-        className={`fixed w-full sm:w-96 3xl:w-[800px] bg-white  ${
+        className={`fixed w-full sm:w-[500px] 3xl:w-[800px] bg-white  ${
           props.showSidebar ? "translate-x-0" : "translate-x-full"
         } flex flex-col justify-center items-center h-full transition-all duration-[600ms] z-10`}
       >
         <ul>
           <li>
-            <Giant text="About" className="mb-1" />
+            <Giant text="About" className="mb-2" />
             <CodecUnderline text="01" className="mb-3" />
           </li>
           <li>
-            <Giant text="Meow" className="mb-1" />
+            <Giant text="Experience" className="mb-2" />
             <CodecUnderline text="02" className="mb-3" />
           </li>
           <li>
-            <Giant text="Hackathons" className="mb-1" />
+            <Giant text="Hackathons" className="mb-2" />
             <CodecUnderline text="03" className="mb-3" />
           </li>
           <li>
-            <Giant text="Projects" className="mb-1" />
+            <Giant text="Projects" className="mb-2" />
             <CodecUnderline text="04" className="mb-3" />
           </li>
           <li>
-            <Giant text="Contact" className="mb-1" />
+            <Giant text="Contact" className="mb-2" />
             <CodecUnderline text="05" className="mb-3" />
           </li>
         </ul>
