b1d4abae3653ea5871ccba788590ee93c5099368
Xavier D'Mello <xaviermdmello@gmail.com>
2023-07-21 08:58:11 -0400

    boxy things

--- src/components/slides/2About.tsx
+++ src/components/slides/2About.tsx
@@ -2,7 +2,7 @@ import Portrait from "../../assets/portrait.jpg";
 export default function About() {
   return (
     <div className="relative mt-24 mb-24 flex flex-col m-4 sm:dynamicPadding  items-center justify-center">
-      <img className="w-72 sm:w-80   border-highlight border-4" src={Portrait} alt="Portrait of Xavier" />
+      <img className="w-72 sm:w-80   border-black border-2" src={Portrait} alt="Portrait of Xavier" />
       <div className="absolute -z-10 top bg-secondary h-60 sm:w-3/4 w-full "></div>
     </div>
   );
