e24136c141604b96a47aee43308c68441fa0ba16
Xavier DMello <xaviermdmello@gmail.com>
2023-07-21 17:46:31 -0400

    added wip warning

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,16 @@ import About from "./components/slides/2About";
 function App() {
   return (
     <div>
+      <div className="w-full bg-orange-500 text-center">
+        <b>This website is a work in progress! I just like testing in prod :)</b>
+      </div>
       <Header />
 
       {/* First Slide */}
       <Hero />
 
-
-
-
       {/* Second Slide */}
       <About />
-
-
     </div>
   );
 }
