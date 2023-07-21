dede16f653cb6d7f23464a989b726a315e182d55
Xavier DMello <xaviermdmello@gmail.com>
2023-07-21 17:47:36 -0400

    fix wip notice

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,10 @@ import About from "./components/slides/2About";
 function App() {
   return (
     <div>
+      <Header />
       <div className="w-full bg-orange-500 text-center">
         <b>This website is a work in progress! I just like testing in prod :)</b>
       </div>
-      <Header />
-
       {/* First Slide */}
       <Hero />
 
