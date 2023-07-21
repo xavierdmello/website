1b0612b083e3a4c54ad01ba3a6f059eb68313e2f
Xavier D'Mello <xaviermdmello@gmail.com>
2023-07-20 21:06:52 -0400

    order

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
-import Hero from "./components/slides/Hero";
-import About from "./components/slides/About";
+import Hero from "./components/slides/1Hero";
+import About from "./components/slides/2About";
 
 function App() {
   return (
