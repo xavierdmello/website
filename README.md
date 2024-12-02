b4094c9ab0f131fe8450836b9cb6e9cd3a7cc0b8
Xavier D'Mello <xaviermdmello@gmail.com>
2024-12-02 10:21:09 -0500

    redir to github; temp until new website is a thing :)

--- README.md
+++
@@ -1,5 +0,0 @@
-# Personal website
-
-Tech stack: React, Vite, Typescript, Tailwind CSS
-
-Check it out: https://www.xavierdmello.com
--- src/components/Codec.tsx
+++
@@ -1,3 +0,0 @@
-export default function Codec(props: { text: string; className?: string }) {
-  return <h4 className={props.className + ` fade1 transition-all duration-[2000ms]`}>{props.text}</h4>;
-}
