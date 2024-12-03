ba58881d4080b82b745f38009a04e6c780e3a0a3
Xavier D'Mello <xaviermdmello@gmail.com>
2024-12-03 17:32:04 -0500

    fade

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,25 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import { buttonVariants } from "@/components/ui/button";
 import Intro from "@/components/Intro";
 import Work from "@/components/Work";
 import Projects from "@/components/Projects";
+
 export default function Home() {
   return (
     <div className="flex flex-col h-screen max-w-3xl mx-auto pt-36 sm:pt-48 w-[93%] gap-16">
-      <Intro />
-      <Work />
-      <Projects />
+      <div className="animate-fade-in [--animation-delay:0ms]">
+        <Intro />
+      </div>
+
+      <div className="animate-fade-in [--animation-delay:200ms]">
+        <Work />
+      </div>
+
+      <div className="animate-fade-in [--animation-delay:400ms]">
+        <Projects />
+      </div>
     </div>
   );
 }
